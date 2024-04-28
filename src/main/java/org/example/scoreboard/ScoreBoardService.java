f22bdf37403630b770b1ff3509ec976b360b0a52
lszo <lukasz.szota@gmail.com>
2024-04-29 00:32:33 +0200

    Add logic for ScoreBoardService updateScore method tests passed

--- src/main/java/org/example/scoreboard/ScoreBoardService.java
+++ src/main/java/org/example/scoreboard/ScoreBoardService.java
@@ -14,7 +14,16 @@ public class ScoreBoardService {
     }
 
     public void updateScore(TeamScore homeTeamScore, TeamScore awayTeamScore) {
+        Match match = findMatch(homeTeamScore.getTeamName(), awayTeamScore.getTeamName());
+        match.getHomeTeamScore().setScore(homeTeamScore.getScore());
+        match.getAwayTeamScore().setScore(awayTeamScore.getScore());
+    }
 
+    private Match findMatch(final String homeTeam, final String awayTeam) {
+        return scoreBoard.getBoard().stream()
+                .filter(m -> m.getHomeTeamScore().getTeamName().equals(homeTeam) && m.getAwayTeamScore().getTeamName().equals(awayTeam))
+                .findAny()
+                .orElse(null);
     }
 
     public void finishMatch(String homeTeam, String awayTeam) {
